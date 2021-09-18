1c28fb9ec9435d72ba3b891398921f8de4a794cb
Ali Zhao <0604aliz@gmail.com>
2021-09-18 14:21:14 -0400

    added log messages and lifecycle methods

--- app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -2,12 +2,15 @@ package com.example.geoquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends Activity {
+
+    private static final String TAG="QuizActivity";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -29,6 +32,9 @@ public class QuizActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d(TAG, "onCreate(Bundle) called");
+
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView=(TextView)findViewById(R.id.question_text_view);
@@ -83,4 +89,31 @@ public class QuizActivity extends Activity {
         else
             Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "oonDestroy() called");
+    }
+
 }
