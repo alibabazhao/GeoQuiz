ab90f9e5dd32ca676b0c8db321134520f77c3ff5
Ali Zhao <0604aliz@gmail.com>
2021-09-20 18:49:59 -0400

    pass data from QuizActivity to CheatActivity

--- app/src/main/java/com/example/geoquiz/CheatActivity.java
+++ app/src/main/java/com/example/geoquiz/CheatActivity.java
@@ -3,17 +3,39 @@ package com.example.geoquiz;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class CheatActivity extends Activity {
 
+    private boolean mAnswerIsTrue;
+
     private TextView mAnswerTextView;
     private Button mShowAnswerButton;
+
+    public static final String EXTRA_ANSWER_IS_TRUE="com.bignerdranch.android.geoquiz.answer_is_true";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
+
+        mAnswerIsTrue=getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView=(TextView)findViewById(R.id.answerTextView);
+
+        mShowAnswerButton=(Button)findViewById(R.id.showAnswerButton);
+        mShowAnswerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                if(mAnswerIsTrue) {
+                    mAnswerTextView.setText(R.string.true_button);
+                }
+                else {
+                    mAnswerTextView.setText(R.string.false_button);
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -86,6 +86,8 @@ public class QuizActivity extends Activity {
             @Override
             public void onClick(View w){
                 Intent i=new Intent(QuizActivity.this, CheatActivity.class);
+                boolean answerIsTrue=mQuestionBank[mCurrentIndex].isTrueQuestion();
+                i.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
                 startActivity(i);
             }
         });
